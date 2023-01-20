0f9ae39f024fa0a6e00fb5e93d5ff4060baf3ac2
Asif <asifabbas478@gmail.com>
2023-01-20 11:50:04 +0530

    my first Git commit

---
+++ command-line.py
@@ -0,0 +1,5 @@
+import sys
+if __name__ =="__main__":
+    print('That this script is being executed using the `python` interpreter')
+    print('The arguments to this script are :')
+    print(sys.argv)
